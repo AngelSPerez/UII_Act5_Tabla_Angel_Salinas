9b3043bef902124f5307e46789be802777f2eb12
AngelSPerez <a.23308051281160@cbtis128.edu.mx>
2025-10-07 08:30:08 -0600

    version 2

--- app_cliente/models.py
+++ app_cliente/models.py
@@ -4,6 +4,7 @@ class Cliente(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     edad = models.CharField(max_length=100)
+    fecha = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.nombre} {self.apellido}"
+        return f"{self.nombre} {self.apellido} {self.edad}"
